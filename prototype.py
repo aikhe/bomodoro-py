e7c89be74ae0e1afc299ced0664cd57a3a588824
Luoie8340 <itslukiee@gmail.com>
2024-04-04 20:56:39 +0800

    bomb animation started

--- prototype.py
+++ prototype.py
@@ -1,3 +1,13 @@
-from customtkinter import ctk
+import customtkinter as ctk
 
-root = ctk.CTk()
+class Bomb:
+    def __init__(self, parent) -> None:
+        self.parent = parent
+
+if __name__ == "__main__":
+    root = ctk.CTk()
+    root.geometry("430x430")
+
+    Bomb(root)
+
+    root.mainloop()
